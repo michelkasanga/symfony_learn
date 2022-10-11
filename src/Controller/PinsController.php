6bf7b8cf54b7cb591ef81d7533900ef617ba1ee5
michelKasanga <kasangamichel1@gmail.com>
2022-10-11 18:06:33 +0200

    v1.0.1

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -5,16 +5,16 @@ namespace App\Controller;
 use App\Entity\Pins;
 use App\Repository\PinsRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     private $entityManager,
-            $pinRepos,
-            $entityPin;
+    $pinRepos,
+        $entityPin;
 
     public function __construct(EntityManagerInterface $entityManager, PinsRepository $pinRepos)
     {
@@ -23,28 +23,34 @@ class PinsController extends AbstractController
         $this->entityPin = new Pins;
 
     }
-    #[Route('/', name: 'app_pins')]
+    #[Route('/', name:'app_pins')]
     public function index(): Response
     {
-        $pins = $this->pinRepos->findAll();
-        return $this->render('pins/index.html.twig', compact('pins'));
+    $pins = $this->pinRepos->findAll();
+    return $this->render('pins/index.html.twig', compact('pins'));
     }
 
-    #[Route('/create', name: 'app_create', methods: ['POST', 'GET'])]
-    public function create(Request $request):Response
-    {
-        $form = $this->createFormBuilder($this->entityPin)
-                ->add('title',null,['attr'=>['class'=>'form-controle','autofocus'=>true]])
-                ->add('content',null,['attr'=>['row'=>5,'col'=>5]])
-                ->getForm()
-        ;
+    #[Route('pin/{id}',methods:['GET'])]
+    public function  show(int $id){
+        $pin = $this->pinRepos->find($id);
+        return $this->render('pins/show.html.twig', compact('pin'));
+    }
 
-            $get = $form->createView() ;
-            $form->handleRequest($request);
-            if($form->isSubmitted() && $form->isValid()){
-                $this->pinRepos->save($this->entityPin, true);
-                return $this->redirectToRoute('app_pins');
-            }   
-        return $this->render('pins/create.html.twig', compact('get'));
+    #[Route('pin/create', priority:1, name:'app_create', methods:['POST', 'GET'])]
+    public function create(Request $request): Response
+    {
+    $form = $this->createFormBuilder($this->entityPin)
+        ->add('title', null, ['attr' => ['class' => 'form-controle', 'autofocus' => true]])
+        ->add('content', null, ['attr' => ['row' => 5, 'col' => 5]])
+        ->getForm()
+    ;
+    $get = $form->createView();
+
+    $form->handleRequest($request);
+    if ($form->isSubmitted() && $form->isValid()) {
+        $this->pinRepos->save($this->entityPin, true);
+        return $this->redirectToRoute('app_pins');
+    }
+    return $this->render('pins/create.html.twig', compact('get'));
     }
 }
